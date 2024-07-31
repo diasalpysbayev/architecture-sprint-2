88a8298d31eb3f28dbed0422b597f0472a456086
diasalpysbayev <alpysbayevdias01@gmail.com>
2024-07-31 15:27:15 +0500

    cache

--- sharding-repl-cache/scripts/mongo-init.sh
+++ sharding-repl-cache/scripts/mongo-init.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-#docker exec -it redis_1 redis_1<<EOF
-#echo "yes" | redis-cli --cluster create   173.17.0.2:6379   173.17.0.3:6379   173.17.0.4:6379   173.17.0.5:6379   173.17.0.6:6379   173.17.0.7:6379   --cluster-replicas 1
-#EOF
+docker exec -it redis_1 redis_1<<EOF
+echo "yes" | redis-cli --cluster create   173.17.0.2:6379   173.17.0.3:6379   173.17.0.4:6379   173.17.0.5:6379   173.17.0.6:6379   173.17.0.7:6379   --cluster-replicas 1
+EOF
 
 docker exec -it mongodb2 mongosh <<EOF
 rs.initiate({_id: "rs0", members: [
